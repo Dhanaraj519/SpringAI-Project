df2d507380b77e5369acca6df91dc3d10f76f629
Dhanaraj519 <panabakadhanaraj4@gmail.com>
2025-07-07 00:44:14 +0530

    commit successfully

---
+++ SpringAI-Backend/src/main/java/com/springAI/backend/SpringAI_Backend/SpringAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springAI.backend.SpringAI_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiBackendApplication.class, args);
+	}
+
+}
---
+++ SpringAI-Backend/src/test/java/com/springAI/backend/SpringAI_Backend/SpringAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAI.backend.SpringAI_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
